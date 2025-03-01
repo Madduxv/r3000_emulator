a074224db91010349e503f735e8e532d0ef5c72d
Madduxv <madduxvjvitrano@gmail.com>
2025-03-01 14:31:45 -0600

    Added symbols helper file

---
+++ include/assembler/symbols.hpp
@@ -0,0 +1,12 @@
+#ifndef SYMBOLS_HPP
+#define SYMBOLS_HPP
+
+#include <map>
+#include <string>
+
+extern std::map<std::string, uint16_t> r_type;
+extern std::map<std::string, uint16_t> j_type;
+extern std::map<std::string, uint16_t> i_type;
+extern std::map<std::string, uint16_t> registers;
+
+#endif // !SYMBOLS_HPP
